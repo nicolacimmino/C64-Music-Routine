18e78790a9436ee8153af5848d43d81688d08b72
Nicola Cimmino <nicola.cimmino@gmail.com>
2018-09-15 12:02:17 +0100

    Keep images size consistent

--- tests/plotwav.m
+++ tests/plotwav.m
@@ -1,9 +1,10 @@
 function plotwav(filename)
-pwd
-[y,fs] = audioread(filename);
-time=1000*(1:length(y))/fs;
-figure;
-plot(time,y);
-grid on;
-set (gca, "xminorgrid", "on");
+  [y,fs] = audioread(filename);
+  time=1000*(1:length(y))/fs;
+  figure(1, 'position',[0,0,800,300]);
+  plot(time,y);
+  grid on;
+  set (gca, "xminorgrid", "on");
+  png_filename=strrep(filename,'.wav','.png');
+  print(png_filename, '-dpng', '-S800,300');
 endfunction
